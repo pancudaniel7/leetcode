acaf0fec649ed68b8b9e158aa4ae3ea02be417a7
Pancu Daniel <pancudaniel@Pancus-MacBook-Pro.local>
2024-11-05 00:41:31 +0200

    Is polindrom?

---
+++ src/IsPolindrom.java
@@ -0,0 +1,33 @@
+// url: https://leetcode.com/problems/palindrome-number/
+
+public class IsPolindrom {
+    static class Solution {
+
+        private boolean isPolindrom(int x) {
+            String number = String.valueOf(x);
+            int length = number.length();
+
+            if (length == 1) {
+                return true;
+            }
+
+            boolean isPolindrom = true;
+            for (int i = 0; i < length / 2; i++) {
+                if (number.charAt(i) != number.charAt(length - i - 1)) {
+                    isPolindrom = false;
+                }
+            }
+
+            return isPolindrom;
+        }
+
+
+        public static void main(String[] args) {
+            int x = 121;
+            Solution solution = new Solution();
+
+            boolean result = solution.isPolindrom(x);
+            System.out.println(result);
+        }
+    }
+}
