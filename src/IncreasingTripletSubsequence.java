e5f8ad6b31ff8584fe6a9592774086e350b4e793
Daniel Pancu <pancudaniel7@gmail.com>
2025-05-28 01:42:47 +0300

    IncreasingTripletSubsequence

---
+++ src/IncreasingTripletSubsequence.java
@@ -0,0 +1,32 @@
+// url: https://leetcode.com/problems/increasing-triplet-subsequence/description/?envType=study-plan-v2&envId=leetcode-75
+public class IncreasingTripletSubsequence {
+
+    public boolean increasingTriplet(int[] nums) {
+        int first = Integer.MAX_VALUE;
+        int second = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+            if (num <= first) {
+                first = num;
+            } else if (num <= second) {
+                second = num;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        IncreasingTripletSubsequence increasingTripletSubsequence = new IncreasingTripletSubsequence();
+//        boolean result = increasingTripletSubsequence.increasingTriplet(new int[]{2, 1, 5, 0, 4, 6});
+        boolean result = increasingTripletSubsequence.increasingTriplet(new int[]{5, 4, 3, 2, 1});
+//        boolean result = increasingTripletSubsequence.increasingTriplet(new int[]{5});
+//        boolean result = increasingTripletSubsequence.increasingTriplet(new int[]{5, 4});
+//        boolean result = increasingTripletSubsequence.increasingTriplet(new int[]{1, 2, 3});
+//        boolean result = increasingTripletSubsequence.increasingTriplet(new int[]{1, 2, 5, 4});
+
+//        boolean result = increasingTripletSubsequence.increasingTriplet(new int[]{20, 100, 10, 12, 5, 13});
+        System.out.println(result);
+    }
+}
