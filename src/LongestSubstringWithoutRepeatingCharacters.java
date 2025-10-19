b6cdac6f07709559543ac3adc632240fc982eba0
Daniel Pancu <pancudaniel7@gmail.com>
2025-10-19 23:23:33 +0300

    Changed LogestSubstringWithoutRepeatingCharacters

--- src/LongestSubstringWithoutRepeatingCharacters.java
+++ src/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,43 +1,34 @@
 // url: https://leetcode.com/problems/longest-substring-without-repeating-characters/
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 
 public class LongestSubstringWithoutRepeatingCharacters {
     static class Solution {
 
         private static int getSubstringLength(String s) {
-            Map<Character, Integer> collector = new HashMap<>();
-
-            int output = 0, counter = 0;
-            for (int i = 0; i < s.length(); i++) {
-                char character = s.charAt(i);
-                if (collector.containsKey(character)) {
-                    i = collector.get(character);
-                    if (counter > output) {
-                        output = counter;
-                    }
-                    counter = 0;
-                    collector.clear();
-                } else {
-                    collector.put(character, i);
-                    counter++;
+            int[] last = new int[128];
+            Arrays.fill(last, -1);
+            int l = 0, ans = 0;
+            for (int r = 0; r < s.length(); r++) {
+                int c = s.charAt(r);
+                if (last[c] >= l) {
+                    l = last[c] + 1;
                 }
+                last[c] = r;
+                ans = Math.max(ans, r - l + 1);
             }
-            if (counter > output) {
-                output = counter;
-            }
-            return output;
+            return ans;
         }
 
         public static void main(String[] args) {
 //            String s = "abcabcbb";
 //            String s = "bbbbbbbb";
-//            String s = "pwwkew";
+            String s = "pwwkew";
 //            String s = "dvdf";
-            String s = "aab";
+//            String s = "aab";
 
             int length = getSubstringLength(s);
+            System.out.println(length);
 
         }
     }
